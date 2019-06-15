7aae9144070d4242ab1f518227cec6d81eb685a6
bdraihan71 <bdraihan71@gmail.com>
2019-06-15 15:12:02 +0600

    add new field in user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,32 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('f_name');
+            $table->string('l_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('phone');
+            $table->enum('source', ['web', 'app']);
+            $table->enum('type');
             $table->string('password');
+            $table->string('is_premium');
+            $table->date('birthday');
+            $table->enum('gender', ['male', 'female', 'other']);
+            $table->enum('marital_status', ['Married', 'Single', 'No Marital Status']);
+            $table->string('active');
+            $table->string('session');
+            $table->string('fb_id');
+            $table->string('google_id');
+            $table->string('twitter_id');
+            $table->string('subscription');
+            $table->string('warnings');
+            $table->string('blocked');
+            $table->string('registered');
+            $table->string('track_download_id');
+            $table->string('partner_id');
+            $table->string('location_id');
+            $table->string('device_id');
+            $table->string('fcm_id');
             $table->rememberToken();
             $table->timestamps();
         });
