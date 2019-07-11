a57583838ca49fbc2a82ba2c903f9cb34e75f30b
bdraihan71 <raihan@techynafdev.com>
2019-07-11 20:14:54 +0600

    change user seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,6 +14,8 @@ class UsersTableSeeder extends Seeder
         DB::table('users')->insert([
             'id' => 1,
             'email' => 'bdraihan71@gmail.com',
+            'f_name' => 'Raihan',
+            'l_name' => 'Farhad',
             'email_verified_at' => new DateTime,
             'password' => bcrypt('bangladesh')
         ]);
