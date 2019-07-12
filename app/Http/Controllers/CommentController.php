ea5cdb415b02db88741bc7867a98549aac0e3f9f
bdraihan71 <raihan@techynafdev.com>
2019-07-12 18:09:04 +0600

    comment done

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -4,13 +4,32 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Comment;
+use Auth;
 
 class CommentController extends Controller
 {
-    public function index()
+    public function store(Request $request)
     {
-        $comments = Comment::latest()->get();
-        return view('post.index', compact('comments'));
+        $this->validate($request, [
+            'body' => 'required',
+        ]);
 
+        $is_anonymous = $request->get('is_anonymous') ? 1 : 0;
+        $comment = new Comment([
+            'body' => $request->get('body'),
+            'user_id' => Auth::id(),
+            'source' => $request->get('source'),
+            'is_anonymous' =>  $is_anonymous,
+            'post_id' => $request->get('post_id'),
+        ]);
+        $comment->save();
+        return redirect()->back();
+    }
+
+    public function destroy($id)
+    {
+        $comment = Comment::find($id);
+        $comment->delete();
+        return redirect()->back();
     }
 }
