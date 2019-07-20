21639b0d8ed12424ff4e0ca0180bc7c56191850d
bdraihan71 <raihan@techynafdev.com>
2019-07-20 20:16:58 +0600

    commit pic fix

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -17,7 +17,7 @@
                             </div>
                             <div class="form-inline float-right">
                                 <div class="form-check mr-2">
-                                    <input type="checkbox" name="is_anonymous" value="1" class="form-check-input" id="exampleCheck1">
+                                    <input type="checkbox" name="is_anonymous" value="1" class="form-check-input" id="exampleCheck1" checked>
                                     <label class="form-check-label text-dark" for="exampleCheck1">Hide My Name</label>
                                 </div>
                                 <button class="btn btn-success ripple ">Submit Question</button>
@@ -35,22 +35,31 @@
             @foreach ($posts as $post)
                 <div class="card mb-2 card-2">
                         <div class="card-body" id="myanchorid{{ $count }}">
-                            <p>{{  $post->is_anonymous == 0 ? $post->user->f_name .' '.$post->user->l_name : 'Anonymous ' }}</p>
-                            <h4>{{ $post->body }}</h4>
+                            <div class="cardinfo mb-3">
+                                <img src="{{ $post->is_anonymous == 1 ? '/img/anonymous.png' : ($post->user->gender == 'female' ? '/img/female.png' : ($post->user->gender == 'male' ? '/img/male.png' : '/img/other.png'))}}" class="float-left" >
+                                <p class="d-inline ml-2">{{  $post->is_anonymous == 0 ? $post->user->f_name .' '.$post->user->l_name : 'Anonymous ' }}</p><br>
+                                <p class="d-inline ml-2">{{ $post->created_at }}</p>
+                            </div>
+                            <h5 class="mp-5 ">{{ $post->body }}</h5>
                             <hr>
                             @if(Auth::check())
-                                <button onclick="myFunction({{ $count }}, 'comment', 'reply', {{ $replycount }})" class="btn default">Comment</button>
+                                <button class="btn btn-white"><a style=" text-decoration: none !important; color:black" href="#">Like</a></button>
+                                <button onclick="myFunction({{ $count }}, 'comment', 'reply', {{ $replycount }})" class="btn btn-white">Comment</button>
+                                <button class="btn btn-white"><a style=" text-decoration: none !important; color:black" href="#">Share</a></button>
                             @else
+                                <button class="btn default"><a style=" text-decoration: none !important; color:black" href="{{ route('login') }}">Like</a></button>
                                 <button class="btn default"><a style=" text-decoration: none !important; color:black" href="{{ route('login') }}">Comment</a></button>
+                                <button class="btn default"><a style=" text-decoration: none !important; color:black" href="{{ route('login') }}">Share</a></button>
                             @endif
                             @if(Auth::check() && Auth::id() == $post->user->id)
                                 <form action="{{ route('post.destroy', ['post' => $post->id])}}" onclick="return confirm('Are you sure, you want to delete this post?')" method="post" style="display: inline;">
                                     @csrf
                                     @method('delete')
                                     <input type="hidden" name="id" value="#myanchorid{{ $count}}">
-                                    <button class="btn btn-danger">Delete</button>
+                                    <button class="btn btn-white text-danger">Delete</button>
                                 </form>
                             @endif
+                            <hr>
                             {{-- commet --}}
                             <div class="container" id="myDIVcomment{{ $count }}reply{{ $replycount }}" style="display:none">
                                 <form  method="post" action="{{ route('comment.store') }}"  class="text-muted m-2">
@@ -61,17 +70,21 @@
                                     <div class="form-group">
                                         <textarea class="form-control" name="body" id="body" rows="2"></textarea>
                                     </div>
-                                    <div class="form-check mb-2">
-                                        <input type="checkbox" name="is_anonymous" value="1" class="form-check-input" id={{ $post->id }}>
-                                        <label class="form-check-label" for={{ $post->id }}>Hide My Name</label>
+                                    <div class="form-inline float-right ">
+                                        <div class="form-check mr-2">
+                                            <input type="checkbox" name="is_anonymous" value="1" class="form-check-input" id={{ $post->id }}>
+                                            <label class="form-check-label text-dark" for={{ $post->id }}>Hide My Name</label>
+                                        </div>
+                                        <button class="btn btn-success ripple ">Submit Comment</button>
                                     </div>
-                                    <button class="btn btn-outline-success btn-block">Submit Comment</button>
                                 </form>
                             </div>
-                            <div class="mt-3">
+                            <div class="mt-5">
                                 @foreach ($post->comments as $comment)
-                                    <h6>
-                                        <div class="mb-2 d-inline"><b><i>{{  $comment->is_anonymous == 0 ? $comment->user->f_name .' '.$comment->user->l_name : 'Anonymous' }}</i> </b>{{ '  '. $comment->body }}</div>
+                                        <div class="cardcomment mb-3">
+                                                <img class="mr-2" src="{{ $comment->is_anonymous == 1 ? '/img/anonymous.png' : ($comment->user->gender == 'female' ? '/img/female.png' : ($comment->user->gender == 'male' ? '/img/male.png' : '/img/other.png'))}}" class="float-left" >
+                                                <div class="d-inline commentbody"><b>{{  $comment->is_anonymous == 0 ? $comment->user->f_name .' '.$comment->user->l_name : 'Anonymous' }}</b>{{ '  '. $comment->body }}</div>
+                                        </div>
                                         @if(Auth::check())
                                             <button onclick="myFunction({{ $count }}, 'comment', 'reply', {{ $comment->id }})" class="btn default">Reply</button>
                                         @else
@@ -85,7 +98,6 @@
                                                 <button class="btn btn-danger">Delete</button>
                                             </form>
                                         @endif
-                                    </h6>
                                     {{-- replay --}}
                                     <div class="container" id="myDIVcomment{{ $count }}reply{{ $comment->id }}" style="display:none">
                                         <form  method="post" action="{{ route('reply.store') }}"  class="text-muted m-2">
